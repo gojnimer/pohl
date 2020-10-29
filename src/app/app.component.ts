83f1fb4c3ef29c92d94cfa996d5a567ef452ac73
gojnimer <gojnimer@gmail.com>
2020-10-29 00:12:58 -0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { HierarchicalAnimation } from './route-animation';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pohl';
+  showUp = false;
 
   getDepth(outlet) {
     return outlet.activatedRouteData['depth'];
-}
+  }
 
-goToPortfolio(){
  
-}
+  @HostListener('window:scroll', ['$event']) // for window scroll events
+  onScroll(event) {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      this.showUp = true;
+    } else {
+      this.showUp = false;
+    }
+  }
+
+  goUp(){
+    document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
 
 }
--- src/app/templates/fluid/fluid.component.ts
+++ src/app/templates/fluid/fluid.component.ts
@@ -1,5 +1,5 @@
 import { TemplateService } from './../template.service';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
@@ -7,30 +7,33 @@ import { ActivatedRoute, Router } from '@angular/router';
   templateUrl: './fluid.component.html',
   styleUrls: ['./fluid.component.css']
 })
-export class FluidComponent implements OnInit,OnDestroy {
+export class FluidComponent implements OnInit, OnDestroy {
   classe = '';
-  gallery:any[];
+  gallery: any[];
 
-  constructor(private router:Router,private ts:TemplateService) { }
+  constructor(private router: Router, private ts: TemplateService) { }
 
+
+ 
   ngOnInit(): void {
     let e = history.state;
     console.log(e);
-    if(e?.reload){
+    if (e?.reload) {
       window.location.reload();
     }
-    this.ts.getImageList().subscribe((e:any) => {
+    this.ts.getImageList().subscribe((e: any) => {
       this.gallery = e;
-       setTimeout(() => this.classe = 'selected',100);
-      },error => console.log('error',error))
- 
-    
+      setTimeout(() => this.classe = 'selected', 100);
+    }, error => console.log('error', error))
 
-    
+
+
+
 
   }
+  
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     this.classe = '';
   }
 
